c82ae093ed99e16ab86344f3eda737439a1f4cb6
Andre <akers.andre@yahoo.com>
2024-04-27 12:14:28 -0700

    03 in progress

--- employee.js
+++ employee.js
@@ -1,14 +1,23 @@
 class Employee {
-    constructor(name, salary, title, manager = null) {
+    constructor(name, salary, title, manager) {
         this.name = name
         this.salary = salary
         this.title = title
         this.manager = manager
+        if (manager) {
+            manager.addEmployee(name)
+        }
     }
 }
 
-const leo = new Employee('Leonardo', 90000, 'Ninja');
-console.log(leo)
+// Make sure the manager exsist
+// If the manager exsist, want to call addEmployee() when we create an employee
+// Call addEmployee inside of the contructor body
+/* if (manager exsist) call addEmployee(this.manager) will run and push the manager to the 
+specified managers array, dont need to have another statement because manager is already
+sent to null.
+*/
+
 
 try {
     module.exports = Employee
--- manager.js
+++ manager.js
@@ -1,7 +1,7 @@
 const Employee = require('./employee')
 
 class Manager extends Employee {
-  constructor(name, salary, title, manager = null, employees = []) {
+  constructor(name, salary, title, manager, employees = []) {
     super(name, salary, title, manager)
     this.employees = employees
   }
@@ -12,17 +12,12 @@ class Manager extends Employee {
 
 }
 
-const splinter = new Manager('Splinter', 100000, 'Sensai');
+const splinter = new Manager('Splinter', 100000, 'Sensei');
 console.log('Before: ', splinter);
 
 const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
 const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
 const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
 const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
-
-splinter.addEmployee(leo);
-splinter.addEmployee(mikey);
-splinter.addEmployee(donnie);
-splinter.addEmployee(raph);
 
 console.log('After: ', splinter);
