b8b877758e2a70c88d7d303e8b33e8b857913712
Andre <akers.andre@yahoo.com>
2024-04-27 11:14:12 -0700

    Phase 01 Done

---
+++ employee.js
@@ -0,0 +1,17 @@
+class Employee {
+    constructor(name, salary, title, manager = null) {
+        this.name = name
+        this.salary = salary
+        this.title = title
+        this.manager = manager
+    }
+}
+
+const leo = new Employee('Leonardo', 90000, 'Ninja');
+console.log(leo)
+
+try {
+    module.exports = Employee
+} catch {
+    module.exports = null
+}
