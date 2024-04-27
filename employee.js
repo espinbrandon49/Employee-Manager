6d134ce6466af44c31cc8f620ba069f83cf7a649
Brandon Espinosa <espinbrandon49@gmail.com>
2024-04-27 19:46:36 -0400

    create calculate bonus function

--- employee.js
+++ employee.js
@@ -1,26 +1,23 @@
 class Employee {
-    constructor(name, salary, title, manager = null) {
-        this.name = name
-        this.salary = salary
-        this.title = title
-        this.manager = manager
-        if (manager) {
-            manager.addEmployee(this)
-        }
+  constructor(name, salary, title, manager = null) {
+    this.name = name
+    this.salary = salary
+    this.title = title
+    this.manager = manager
+    if (manager) {
+      manager.addEmployee(this)
     }
+  }
+
+  calculateBonus(multiplier) {
+    let bonus = this.salary * multiplier
+    return bonus
+  }
+
 }
 
-// Make sure the manager exsist
-// If the manager exsist, want to call addEmployee() when we create an employee
-// Call addEmployee inside of the contructor body
-/* if (manager exsist) call addEmployee(this.manager) will run and push the manager to the
-specified managers array, dont need to have another statement because manager is already
-sent to null.
-*/
-
-
 try {
-    module.exports = Employee
+  module.exports = Employee
 } catch {
-    module.exports = null
+  module.exports = null
 }
--- manager.js
+++ manager.js
@@ -10,17 +10,53 @@ class Manager extends Employee {
     this.employees.push(employee)
   }
 
+  calculateBonus(multiplier) {
+    return (this.salary + this._totalSubSalary()) * multiplier
+  }
+
+  _totalSubSalary() {
+    let sum = 0
+    const employees = this.employees
+
+    if (!(this instanceof Manager) || !employees) {
+      sum += this.salary
+      return sum
+    }
+
+    // base case
+    if (employees.length === 0) return sum
+
+    // current employee - last employee in the employees array
+    let currentEmployee = employees[employees.length - 1]
+
+    // test to see if the current employee has employees
+    if (currentEmployee.employees) {
+      // if they have employees, loop through the employees array and add each employee's salary to the sum
+      currentEmployee.employees.forEach(el => {
+        sum += el.salary
+      })
+      // then add the current employee's salary to the sum
+      sum += currentEmployee.salary
+    } else {
+      // if the current employee does not have anymore employees add their salary to the sum
+      sum += currentEmployee.salary
+      return sum
+    }
+
+    // remove the current employee from the array
+    employees.pop()
+
+    return sum += this._totalSubSalary()
+  }
 }
 
-const splinter = new Manager('Splinter', 100000, 'Sensei');
-console.log('Before: ', splinter);
+splinter = new Manager('Splinter', 100000, 'Sensei');
+leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+mikey = new Employee('Michelangelo', 85000, 'Grasshopper', leo);
 
-const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
-const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
-const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
-const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
-
-console.log('After: ', splinter);
+console.log(mikey.calculateBonus(0.05)) //employee 4250
+console.log(leo.calculateBonus(0.05)) //manager 8750
+console.log(splinter.calculateBonus(0.05)) //manager 13750
 
 try {
   module.exports = Manager
